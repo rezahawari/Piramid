2cd3fa580f7d05d1384beb30d7820acd8a6835fe
rezahawari <rezahawari19@gmail.com>
2026-08-23 17:36:49 +0700

    feat: add Midtrans configuration file and update environment variables with sandbox credentials

--- config/midtrans.php
+++ config/midtrans.php
@@ -1,8 +1,9 @@
 <?php
 
 return [
-    'server_key' => env('MIDTRANS_SERVER_KEY', ''),
-    'client_key' => env('MIDTRANS_CLIENT_KEY', ''),
+    'merchant_id' => env('MIDTRANS_MERCHANT_ID', 'G778086072'),
+    'server_key' => env('MIDTRANS_SERVER_KEY', 'SB-Mid-server-UWdxhKL11SJqG3c8T0TFyfvo'),
+    'client_key' => env('MIDTRANS_CLIENT_KEY', 'SB-Mid-client-yq36YrCJTtq0Cb2A'),
     'is_production' => env('MIDTRANS_IS_PRODUCTION', false),
     'is_sanitized' => true,
     'is_3ds' => true,
